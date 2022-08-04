b80829d99159301b3fb409a6d3d9e42f52bd865c
johnbridge180 <johnbridge180@gmail.com>
2022-08-04 05:38:50 -0400

    moved C code to new target (type: Command Line Tool)

---
+++ MediaOrganizer/MediaOrganizerCLI/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  MediaOrganizerCLI
+//
+//  Created by John Bridge on 8/3/22.
+//
+
+#include <stdio.h>
+#include "organizer.h"
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
